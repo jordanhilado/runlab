4932da5d178e12843c2c3c896b93739cbaa4a761
Jordan Hilado <jordanalihilado@gmail.com>
2026-03-05 00:20:20 -0800

    Initial Commit

---
+++ runlab/runlabApp.swift
@@ -0,0 +1,17 @@
+//
+//  runlabApp.swift
+//  runlab
+//
+//  Created by Jordan Hilado on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct runlabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
